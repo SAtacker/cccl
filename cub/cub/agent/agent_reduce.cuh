57ff3afa4b2a50442a2c707d1d9c85581b5524c1
Shreyas Atre <shreyasa@nvidia.com>
2024-07-23 21:49:08 -0700

    Try making __shared__ for abs max value
    Signed-off-by: Shreyas Atre <shreyasa@nvidia.com>

--- cub/cub/agent/agent_reduce.cuh
+++ cub/cub/agent/agent_reduce.cuh
@@ -632,18 +632,20 @@ private:
         items[i] = transform_op(input_items[i]);
       }
 
-      InputT abs_max_val = -1;
+      __shared__ InputT abs_max_val[BLOCK_THREADS];
+
+      abs_max_val[threadIdx.x] = -1;
 
 #pragma unroll
       for (int i = 0; i < ITEMS_PER_THREAD; ++i)
       {
-        auto abs_f  = fabs(items[i]);
-        abs_max_val = fmax(abs_f, abs_max_val);
+        auto abs_f               = fabs(items[i]);
+        abs_max_val[threadIdx.x] = fmax(abs_f, abs_max_val[threadIdx.x]);
       }
 
       // Reduce items within each thread stripe
-      thread_aggregate =
-        internal::ThreadReduce(items, reduction_op, thread_aggregate, Int2Type<ITEMS_PER_THREAD>{}, abs_max_val);
+      thread_aggregate = internal::ThreadReduce(
+        items, reduction_op, thread_aggregate, Int2Type<ITEMS_PER_THREAD>{}, abs_max_val[threadIdx.x]);
     }
   }
 
