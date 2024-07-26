73f51aee06d669369f803b0f089be7cd08cc2ca6
Shreyas Atre <shreyasa@nvidia.com>
2024-07-26 14:32:42 -0700

    Try bc
    Signed-off-by: Shreyas Atre <shreyasa@nvidia.com>

--- cub/cub/agent/agent_reduce.cuh
+++ cub/cub/agent/agent_reduce.cuh
@@ -625,24 +625,24 @@ private:
         vec_items[i] = d_vec_in[BLOCK_THREADS * i];
       }
 
-      std::remove_reference_t<decltype(transform_op(input_items[0]))> items[ITEMS_PER_THREAD];
-#pragma unroll
-      for (int i = 0; i < ITEMS_PER_THREAD; ++i)
-      {
-        items[i] = transform_op(input_items[i]);
-      }
+      //       std::remove_reference_t<decltype(transform_op(input_items[0]))> items[ITEMS_PER_THREAD];
+      // #pragma unroll
+      //       for (int i = 0; i < ITEMS_PER_THREAD; ++i)
+      //       {
+      //         items[i] = transform_op(input_items[i]);
+      //       }
 
-      InputT abs_max_val = items[0];
+      //       InputT abs_max_val = items[0];
 
-#pragma unroll
-      for (int i = 1; i < ITEMS_PER_THREAD; ++i)
-      {
-        auto abs_f  = fabs(items[i]);
-        abs_max_val = fmax(abs_f, abs_max_val);
-      }
+      // #pragma unroll
+      //       for (int i = 1; i < ITEMS_PER_THREAD; ++i)
+      //       {
+      //         auto abs_f  = fabs(items[i]);
+      //         abs_max_val = fmax(abs_f, abs_max_val);
+      //       }
 
       // Reduce items within each thread stripe
-      thread_aggregate.add(items, ITEMS_PER_THREAD, abs_max_val);
+      thread_aggregate.add(input_items, ITEMS_PER_THREAD, 1e14);
     }
   }
 
