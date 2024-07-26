9f1de600cdfe14077e63f5ab7c39e88d8244496b
Shreyas Atre <shreyasa@nvidia.com>
2024-07-26 20:32:57 +0000

    As same as Kate
    Signed-off-by: Shreyas Atre <shreyasa@nvidia.com>

--- cub/cub/thread/thread_reduce.cuh
+++ cub/cub/thread/thread_reduce.cuh
@@ -76,13 +76,16 @@ _CCCL_DEVICE _CCCL_FORCEINLINE AccumT ThreadReduceHelper(
   // float4 float4_input[float4_inp_len + 1];
   // auto* float4_input           = reinterpret_cast<cub::CubVector<T, 4>*>(input);
 
-  retval.binned_dmdupdate(input_max, 1, 1);
+  retval.binned_dmdupdate(input_max.x, 1, 1);
 
 #pragma unroll
-  for (int i = 0; i < LENGTH; ++i)
+  for (int i = 0; i < float4_inp_len; ++i)
   {
     // retval         = reduction_op(retval, float4_input);
-    retval.binned_dmddeposit(input[i], 1);
+    retval.binned_dmddeposit(input[i].x, 1);
+    retval.binned_dmddeposit(input[i].y, 1);
+    retval.binned_dmddeposit(input[i].z, 1);
+    retval.binned_dmddeposit(input[i].w, 1);
   }
 
   return retval;
